39d7aac6f3b3bc841f58e5322f7e4c00775a246e
lemur <lemur@DESKTOP-K8A2S7V>
2022-07-25 11:08:43 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semana2_primer_proyecto_visualcsharp_consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
