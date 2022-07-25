8bb3b395426f0abe59df6911f1179f03b9b8d83c
Pedro Antonio Villalta <pavillalta@ugb.edu.sv>
2022-07-25 11:13:55 -0600

    Proyecto inicial subido a GitHub

---
+++ progra2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semana2_primer_proyecto_visualcsharp_consola
+{
+    class progra2
+    {
+    }
+}
